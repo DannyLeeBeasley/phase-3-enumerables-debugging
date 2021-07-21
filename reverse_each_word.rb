7963a762cf745371a690f1807208f389d13b3314
Dan Beasley <dannyleebeasley@gmail.com>
2021-07-21 13:53:14 -0500

    completed assignment

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,8 @@
-# Write your code here
+require 'pry'
+
+def reverse_each_word(sentence)
+    words = sentence.split
+    reversed_words = words.map(&:reverse).join(" ")
+end
+
+puts reverse_each_word("Hey There, Pilgrim!")
